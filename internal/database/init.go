b0604a156c3acf6bf748e47afbc83e622c6ebeef
RudyJakub <kubazewsi@gmail.com>
2025-03-11 15:57:33 +0100

    fixed test db conn

--- internal/database/init.go
+++ internal/database/init.go
@@ -11,19 +11,22 @@ import (
 )
 
 var (
-	database       = os.Getenv("DB_NAME")
-	password       = os.Getenv("DB_PASSWORD")
-	username       = os.Getenv("DB_USERNAME")
-	port           = os.Getenv("DB_PORT")
-	host           = os.Getenv("DB_HOST")
-	postgresClient *pgxpool.Pool
+	dbname             = os.Getenv("DB_NAME")
+	dbnametest         = os.Getenv("DB_NAME_TEST")
+	password           = os.Getenv("DB_PASSWORD")
+	username           = os.Getenv("DB_USERNAME")
+	port               = os.Getenv("DB_PORT")
+	host               = os.Getenv("DB_HOST")
+	postgresClient     *pgxpool.Pool
+	postgresClientTest *pgxpool.Pool
 )
 
 func Connect() *pgxpool.Pool {
+	fmt.Println(username)
 	if postgresClient != nil {
 		return postgresClient
 	}
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", username, password, host, port, database)
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", username, password, host, port, dbname)
 	db, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		log.Fatal(err)
@@ -32,6 +35,32 @@ func Connect() *pgxpool.Pool {
 	if err != nil {
 		log.Fatal(err)
 	}
+	postgresClient = db
+	return db
+}
+
+func ConnectTest() *pgxpool.Pool {
+	if postgresClientTest != nil {
+		return postgresClientTest
+	}
+	fmt.Println(username)
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		"my_website_admin",
+		"123",
+		"localhost",
+		"5432",
+		"my_website_db_test",
+	)
+	db, err := pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	postgresClientTest = db
 	return db
 }
 
--- tests/main_test.go
+++ tests/main_test.go
@@ -1,34 +1,17 @@
 package tests
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"my_website/internal/database"
 	"os"
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	_ "github.com/joho/godotenv/autoload"
 )
 
-var (
-	dbname   = os.Getenv("DB_NAME")
-	password = os.Getenv("DB_PASSWORD")
-	username = os.Getenv("DB_USERNAME")
-	port     = os.Getenv("DB_PORT")
-	host     = os.Getenv("DB_HOST")
-	TestDB   *pgxpool.Pool
-)
+var TestDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
-	var err error
-	fmt.Println(dbname, port, password, username)
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", username, password, host, port, dbname)
-	TestDB, err = pgxpool.New(context.Background(), connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	TestDB = database.ConnectTest()
 	database.InitTables(TestDB, "../internal/database/tables.sql")
 
 	// Run tests
